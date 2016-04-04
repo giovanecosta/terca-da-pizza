6f8660f99c8d43d1bdfc941bcd97b0eef965ad23
giovane <giovane.tomaz@redealumni.com>
2016-04-04 19:42:14 -0300

    Tests server vars

--- index.php
+++ index.php
@@ -27,6 +27,7 @@ if ($allowed){
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <!--  <?php print_r($_SERVER); ?> -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
