dfa046e15923cfe5a1f09ee23ce23ff16aa72617
giovane <giovane.tomaz@redealumni.com>
2017-05-16 13:08:44 -0300

    Messages improvement

--- messages.php
+++ messages.php
@@ -3,6 +3,8 @@
 $today_is_not_tuesday = ['hoje', 'não', 'é', 'terça'];
 shuffle($today_is_not_tuesday);
 
+$fp = !empty($_GET['from_pirata']);
+
 $messages_only_tuesday = [
   'Só rola na Terça... Volta na Terça...',
   'Tem calendário em casa não?! Hoje náo é terça, amiguinho!',
@@ -15,9 +17,9 @@ $messages_only_tuesday = [
   'Chega '.((int) date('Y') + 1).' mas não chega terça? #mimimi',
   'A paciência é uma virtude. Calma que a terça já chega.',
   '#naovaiterpizza',
-  'Terça ainda não chegou, mas você pode fazer a sua: <a href="https://www.youtube.com/watch?v=Z--jYTc8FqM">https://www.youtube.com/watch?v=Z--jYTc8FqM</a>',
+  'Terça ainda não chegou, mas você pode fazer a sua: '. ($fp ? '' : '<a href="https://www.youtube.com/watch?v=Z--jYTc8FqM">' ) .'https://www.youtube.com/watch?v=Z--jYTc8FqM'. ($fp ? '' : '</a>' ),
   'Estudos afirmam que a cada 10 pizzas, metade são 5! Mas hoje não vai ter nenhuma... :/',
-  'Hoje não é terça mas, se você tiver vendo essa messagem, clique <a href="/trolling.jpg" >aqui</a> pra ganhar pizza hoje também!',
+  'Hoje não é terça mas, se você tiver vendo essa messagem, clique '. ($fp ? '' : '<a href="' ) . 'http://tdp.redealumni.lan/ultima_chance.php'. ($fp ? '' : '" >aqui</a>'). ' pra ganhar pizza hoje também!',
   'Guarapari, Búzios, minha arte. Juliana?! Katrina?! 16, 18. Jarbas, o meu pai?! O MEU PAI?! Pizza?! Seu sonho... xD',
   'Hoje é terça! Dia de comer uma pizza! #sqn',
   'Terça é um dia como todos os outros. Só que terça.',
@@ -41,5 +43,5 @@ $messages_closed = [
   '"Chegou cedo pra terça que vem! rsrsrs" - Tio do Pavê.',
   'A pizza já era mas relaxa que tem ovo na geladeira! ;D',
   'Parabéns! Vai manter a dieta com sucesso!',
-  'Os pedidos já fecharam mas, se você tiver vendo essa messagem, clique rápido <a href="/trolling.jpg" >aqui</a> que a gente dá um jeitinho!',
+  'Os pedidos já fecharam mas, se você tiver vendo essa messagem, clique rápido '. ($fp ? '' : '<a href="' ) . 'http://tdp.redealumni.lan/ultima_chance.php'. ($fp ? '' : '" >aqui</a>'). ' que a gente dá um jeitinho!',
 ];
---
+++ ultima_chance.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="utf-8">
+
+    <title>HU3 HU3 HU3 HU3 HU3 BR BR BR BR BR HU3HU3HU3 !!!!111!!!!!</title>
+
+  </head>
+  <body style="text-align: center;">
+
+    <img src="trolling.jpg" alt="Não há nada mais que ue poderia dizer sobre essa fatídica circunstância em que nos encontramos. Aceitar a condição humana de viver numa país onde seus cidadãos tem naturalmente a habilidade de trollar os coleguinhas me parece a solução mais conveniente, ou, quem sabe, se converter a ceita que dói menos. Sorry, Você foi trollado!">
+
+  </body>
+</html>
